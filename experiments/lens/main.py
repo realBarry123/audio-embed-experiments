38c964bed6821d4a9ed85581097f922fee974804
Barry Yu <77864928+realBarry123@users.noreply.github.com>
2026-05-30 15:44:18 -0700

    Experiment logging

--- experiments/lens/main.py
+++ experiments/lens/main.py
@@ -1,15 +1,28 @@
 import os
+import datetime
 import torch
 import soundfile as sf
-from diffusers import StableAudioPipeline
-from nnsight import NNsight
 from nnsight.modeling.diffusion import DiffusionModel
 from dotenv import load_dotenv
 from tqdm import tqdm
 
-load_dotenv()
+if not load_dotenv():
+    raise SystemExit("No .env file found, please make one in the root directory")
+
 CACHE_PATH = os.getenv("CACHE_PATH")
 DEVICE = os.getenv("DEVICE")
+
+if not CACHE_PATH or not DEVICE:
+    raise ValueError("Missing required environment variables: CACHE_PATH, DEVICE")
+
+prompt = input()
+timestamp = str(datetime.datetime.now().strftime("%y-%m-%dT%H:%M:%S"))
+
+# Create folder for results
+if not os.path.exists(timestamp):
+    os.makedirs(timestamp)
+else:
+    raise ZeroDivisionError("Time is moving backwards!")
 
 model = DiffusionModel(
     "stabilityai/stable-audio-open-1.0",
@@ -19,36 +32,10 @@ model = DiffusionModel(
     device_map=DEVICE
 )
 
-# define the prompts
-prompt = "The sound of a hammer hitting a wooden surface."
-negative_prompt = "Low quality."
-
 # set the seed for generator
 generator = torch.Generator(DEVICE).manual_seed(0)
 
 audios = []
-#for i in range(model.text_encoder.config.num_hidden_layers):
-#    print(i)
-#    print(model.text_encoder.encoder.block[i])
-#exit()
-
-# Numerical stability, for no -inf or inf
-# model.text_encoder = model.text_encoder.to(torch.float32)
-
-"""
-audio = model.generate(
-            prompt,
-            num_inference_steps=20, # 200
-            audio_end_in_s=2.0,
-            seed=0
-        )
-
-output = audio[0].T.float().cpu().numpy()
-print("max audio value:", audio.max().item())
-print("max output value:", output.max().item())
-sf.write(f"layers/all.wav", output, model.vae.sampling_rate)
-exit()
-"""
 
 for layer in tqdm(range(model.text_encoder.config.num_hidden_layers)):
     with torch.no_grad():
@@ -59,7 +46,7 @@ for layer in tqdm(range(model.text_encoder.config.num_hidden_layers)):
             seed=0
         ):
             # layer = -1
-            print(f"layer: {layer}")
+            print(f"\n\nlayer: {layer}")
             # replace the final_layer_norm input with the text_encoder's output for the layer.
             hidden_state = model.text_encoder.encoder.block[layer].output[0]#.save()
             print("range of hidden state:", hidden_state.min().item(), hidden_state.max().item())
@@ -68,4 +55,7 @@ for layer in tqdm(range(model.text_encoder.config.num_hidden_layers)):
             audio = model.output.audios[0].save()
             # audios.append(audio)
             audio = audio[0].T.float().cpu().numpy()
-            sf.write(f"experiments/lens/layers/layer{layer}.wav", audio, model.vae.sampling_rate)
+            sf.write(f"experiments/lens/results/{timestamp}/layer{layer}.wav", audio, model.vae.sampling_rate)
+
+with open("experiments/lens/results/report.txt", "a") as f:
+    f.write(f"{timestamp}: \'{prompt}\'")
