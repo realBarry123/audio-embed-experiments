859bdc3dc0a0f3304521503b4fc6798899342fe3
Barry Yu <77864928+realBarry123@users.noreply.github.com>
2026-05-31 14:58:56 -0700

    Update nodiffusers.py

--- experiments/lens/nodiffusers.py
+++ experiments/lens/nodiffusers.py
@@ -1,13 +1,15 @@
-import os
-import sys
+import os, sys
 from dotenv import load_dotenv
+import torch
+import torchaudio
+from einops import rearrange
+from stable_audio_tools import get_pretrained_model
+from stable_audio_tools.inference.generation import generate_diffusion_cond_inpaint
 
-from stable_audio_3 import StableAudioModel
-import soundfile as sf
 
-#if len(sys.argv) != 2:
-#    raise SystemExit(f"Incorrect number of command line arguments")
-#prompt = sys.argv[1]
+if len(sys.argv) != 2:
+    raise SystemExit(f"Incorrect number of command line arguments")
+prompt = sys.argv[1]
 
 if not load_dotenv():
     raise SystemExit("No .env file found, please make one in the root directory")
@@ -18,21 +20,40 @@ DEVICE = os.getenv("DEVICE")
 if not CACHE_PATH or not DEVICE:
     raise ValueError("Missing required environment variables: CACHE_PATH, DEVICE")
 
+if DEVICE == "cuda":
+  model_half = True
 
-model = StableAudioModel.from_pretrained(
-    model_name="medium",
+# Download model
+model, model_config = get_pretrained_model("stabilityai/stable-audio-3-medium")
+sample_rate = model_config["sample_rate"]
+sample_size = model_config["sample_size"]
+
+model = model.to(DEVICE)
+if model_half:
+  model = model.to(torch.float16)
+# Set up text and timing conditioning
+conditioning = [{
+    "prompt": (
+        "A dream-like Synthpop instrumental that would accompany "
+        "a dream-sequence in a surrealist movie 120 BPM"
+    ),
+    "seconds_total": 380
+}]
+
+# Generate stereo audio
+output = generate_diffusion_cond_inpaint(
+    model,
+    steps=8,
+    cfg_scale=1.0,
+    conditioning=conditioning,
+    sample_size=sample_size,
+    sampler_type="pingpong",
     device=DEVICE
 )
-with open("stable-audio-3.txt", "w") as f:
-    f.write(str(model))
-exit()
-audio = model.generate(
-    prompt=(
-        "House music that encapsulates the feeling of being at a festival "
-        "in the sunny weather with all your friends 124 BPM"
-    ),
-    duration=180
-)
 
-audio = audio[0].T.float().cpu().numpy()
-sf.write(f"experiments/lens/stable3.wav", audio, model.vae.sampling_rate)
+# Rearrange audio batch to a single sequence
+output = rearrange(output, "b d n -> d (b n)")
+
+# Peak normalize, clip, convert to int16, and save to file
+output = output.to(torch.float32).div(torch.max(torch.abs(output))).clamp(-1, 1).mul(32767).to(torch.int16).cpu()
+torchaudio.save("experiments/lens/results/output.wav", output, sample_rate)
