b92ac4a4581dd6b5d5a9d111427b314e23323c77
Barry Yu <77864928+realBarry123@users.noreply.github.com>
2026-06-14 15:11:12 -0700

    Create test_sae.py

---
+++ experiments/sae/test_sae.py
@@ -0,0 +1,53 @@
+import os
+import torch
+from torch import nn
+from tqdm import tqdm
+from nnsight.modeling.diffusion import DiffusionModel
+from dotenv import load_dotenv
+from einops import rearrange
+import soundfile as sf
+
+from audembed import datasets, models
+
+if not load_dotenv():
+    raise SystemExit("No .env file found, please make one in the root directory")
+
+CACHE_PATH = os.getenv("CACHE_PATH")
+DEVICE = os.getenv("DEVICE")
+
+if not CACHE_PATH or not DEVICE:
+    raise ValueError("Missing required environment variables: CACHE_PATH, DEVICE")
+
+state_dict, configs, start_epoch = torch.load("experiments/sae/models/sae.pt")
+sae = models.SAE(**configs).to(DEVICE)
+sae.load_state_dict(state_dict)
+
+dataset = datasets.MIRDataset("orchset")
+train_loader, valid_loader = dataset.get_loaders(
+    valid_split=0.2, 
+    batch_size=1
+)
+
+audio = valid_loader[0]
+sf.write(f"experiments/sae/results/test_{dataset.dataset_name}.wav", audio)
+
+diffusion_model = DiffusionModel(
+    "stabilityai/stable-audio-open-1.0",
+    torch_dtype=torch.float32,
+    cache_dir=CACHE_PATH,
+    device_map=DEVICE
+)
+
+def encode_fn(x):
+    with diffusion_model.trace("_"):
+        latent = diffusion_model.vae.encoder(x).save() # [batch, param*channel, frame]
+
+    latent = rearrange(latent, "b (p c) f -> b f c p", c=64, p=2)
+    return latent[..., 1] # (B, F, C=64)
+
+vae_latent = encode_fn(audio)
+
+sae_latent = sae(vae_latent).unsqueeze(0) # (frame, feature_dim=2048)
+print(sae_latent.shape)
+
+torch.save(sae_latent, "experiments/sae/results/{dataset.dataset_name}_sae_latent.pt")
