c809d54652bdd9f56ee2d766c6cdc19679e331fc
Barry Yu <77864928+realBarry123@users.noreply.github.com>
2026-07-01 15:46:22 -0700

    Create sine generation thing

--- audembed/audio.py
+++ audembed/audio.py
@@ -40,3 +40,41 @@ def to_spectrogram(x, target_frames, target_bins, return_complex=False):
     mag = mag[..., :target_bins, 0]
     
     return mag
+
+def generate_audio(freqs):
+
+    def _generate_phase(freqs):
+        """
+        Given a (timesteps, voices)-tensor, output a (time, voices)-tensor
+        of phases where time = timesteps * fs
+        """
+        delta = 2 * torch.pi * freqs / float(44100)
+        initial_phase = torch.rand(freqs.shape[1]) * 2 * torch.pi
+        phase = initial_phase.unsqueeze(0) + torch.cumsum(delta, axis=0)  # start from random phase
+        phase = torch.fmod(phase, 2 * torch.pi) # wrap phase to prevent overflow
+        return phase
+    
+    def _mix(audio):
+        GAIN = 0.4
+        """ 
+        Sum the voice channels of a (channels, time, voices)-tensor, 
+        normalize to int16 
+        """
+        mix = torch.sum(audio, dim=2)
+        mix /= torch.max(torch.abs(mix))
+        return (mix * 32767 * GAIN).to(torch.int16)
+    
+    phase = _generate_phase(freqs)
+    audio = torch.sin(phase)
+    stereo = audio.unsqueeze(0).repeat(2, 1, 1)
+    stereo = _mix(stereo)
+    stereo = stereo.permute(1, 0) # stupid but I have to do this
+
+    return stereo
+
+if __name__ == "__main__":
+    import sounddevice as sd
+    import soundfile as sf
+    audio = generate_audio(torch.tensor([440, 660]).unsqueeze(0).repeat(44100, 1))
+    sd.play(audio.numpy(), samplerate=44100)
+    sd.wait()
